323bc828e81f24536cac9d5d401c3d6dfebf8f48
João Gabriel Borba do Nascimento <108697751+joaogabriel43@users.noreply.github.com>
2026-05-17 10:17:13 -0300

    feat: initial project setup with maven, dependencies, docker-compose and app properties

---
+++ src/main/java/com/flowguard/FlowguardServerApplication.java
@@ -0,0 +1,13 @@
+package com.flowguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowguardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowguardServerApplication.class, args);
+    }
+
+}
