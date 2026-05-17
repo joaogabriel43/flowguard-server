500d369c8d4882f9f0ecf3f45a82fc1ccaa8f651
João Gabriel Borba do Nascimento <108697751+joaogabriel43@users.noreply.github.com>
2026-05-17 12:43:52 -0300

    style: apply tailwind layout and dev proxy configurations

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulse-fast': 'pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+}
